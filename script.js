7d4d00497df683cf38f3493b09d9c962899051a5
ketlynsantos22-ai <ketlyn.santos22@escola.pr.gov.br>
2026-08-28 07:36:51 -0300

    oi

---
+++ script.js
@@ -0,0 +1,7 @@
+const botao = document.querySelector("button");
+botao .addEventlistener("click", botaoclicado);
+
+function botaoclicado(){
+    let texto = botao.querySelector("span");
+texto.textContent++;
+}
